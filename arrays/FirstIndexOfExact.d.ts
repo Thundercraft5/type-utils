803f6796dedd71cdc64bcfc42bf5ac4fdee52a50
Thundercraft5 <69543685+Thundercraft5@users.noreply.github.com>
2022-10-25 21:50:14 -0400

    Update files

--- arrays/FirstIndexOfExact.d.ts
+++
@@ -1,3 +0,0 @@
-import type { InternalIndexOf } from "./InternalIndexOf";
-export type FirstIndexOf<A extends any[], E> = A extends [] ? -1 : undefined extends InternalIndexOf<A, E>[0] ? -1 : InternalIndexOf<A, E>[0];
-//# sourceMappingURL=FirstIndexOfExact.d.ts.map
--- arrays/Push.1.d.ts
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=Push.1.d.ts.map
